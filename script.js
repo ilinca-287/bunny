562d45577d605a09c665c15f73427b7d00bef55e
ilinca-287 <ilinca.ioana.cojocariu@cniasi.ro>
2024-12-01 11:25:40 +0200

    bunny

---
+++ script.js
@@ -0,0 +1,9 @@
+function openacasa() {
+    window.location.href="lectia3.html";
+}
+function openarticole() {
+    window.location.href="articole.html";
+}
+function openpictures() {
+    window.location.href="pictures.html"
+}
